31421635db427adaca86bcbd6bfa9218d5467055
miguelnv22 <miguelnv22@gmail.com>
2019-01-30 19:49:01 +0100

    Modificado proyecto banco.

--- banco_M/src/banco_m/Banco_M.java
+++ banco_M/src/banco_m/Banco_M.java
@@ -15,7 +15,7 @@ public class Banco_M {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Banco.");
     }
     
 }
