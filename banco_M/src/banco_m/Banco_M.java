443343c8ee648e5eec0188254648bf4b807f154c
miguelnv22 <miguelnv22@gmail.com>
2019-02-05 21:47:53 +0100

    Modificación de proyecto banco_M para ejercicio 3.

--- banco_M/src/banco_m/Banco_M.java
+++ banco_M/src/banco_m/Banco_M.java
@@ -15,7 +15,7 @@ public class Banco_M {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Banco.");
+        System.out.println("Banco modificado.");
     }
     
 }
